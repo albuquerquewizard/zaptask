1d3e8d8d9481a4650ec8f80b5e3d15eb1031f542
albuquerquewizard <himanshuwankhadeh@gmail.com>
2025-07-07 20:06:02 +0530

    convex basic setup done

---
+++ convex/schema.ts
@@ -0,0 +1,11 @@
+// convex/schema.ts
+import { defineSchema, defineTable } from "convex/server";
+
+import { v } from "convex/values";
+
+export default defineSchema({
+  todos: defineTable({
+    text: v.string(),
+    completed: v.boolean(),
+  }),
+});
